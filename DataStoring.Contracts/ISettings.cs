dd1125f14747b833166e17c2dfea1a2bbd53290c
Marvin Gerdel <marvin.gerdel@web.de>
2018-12-31 03:53:19 +0100

    Example removed

--- DataStoring.Contracts/ISettings.cs
+++ DataStoring.Contracts/ISettings.cs
@@ -1,10 +1,8 @@
 ﻿using ExtendedIO.SQLiteSupport;
-using System.Collections.Generic;
 
 namespace DataStoring.Contracts
 {
     public interface ISettings : IBaseNode
     {
-        IList<string> Example { get; set; }
     }
 }
--- DataStoring/Settings.cs
+++ DataStoring/Settings.cs
@@ -1,13 +1,9 @@
 ﻿using DataStoring.Contracts;
 using ExtendedIO.SQLiteSupport;
-using SQLiteNetExtensions.Attributes;
-using System.Collections.Generic;
 
 namespace DataStoring
 {
     public class Settings : BaseNode, ISettings
     {
-        [TextBlob("Example")]
-        public IList<string> Example { get; set; }
     }
 }
